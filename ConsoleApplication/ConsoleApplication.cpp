c96f560f46c7cc585d5c3f97c3deec02b4eb21d7
Arlodu <aronlondond@gmail.com>
2020-02-21 20:34:30 -0500

    Added getline to parse an ini file

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -11,11 +12,31 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-    cout << "there are " << argc << " arguments ";
+    cout << "\nthere are " << argc << " arguments \n" << endl;
+
     for (int count{ 0 }; count < argc; ++count)
     {
         cout << count << " " << argv[count] << '\n';
     }
 
+    ifstream config(argv[1]);
+
+    if (config.is_open())
+   {
+        string text;
+        while (getline(config, text))
+        {
+
+            if (text == "Yes")
+            {
+                cout << "the text says yes\n";
+            }
+
+            cout << text << endl;
+
+        }
+        
+        cout << "\nconfig is open\n";
+   }
     return 0;
 }
