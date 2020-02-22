59ce75f8f392d3cc256b90026e8f280b672c6969
testd <testd@DESKTOP-IMB2NDI>
2020-02-21 23:31:06 -0800

    Adding AppCheckTests class and the first test available.

---
+++ ConsoleApplication/AppCheckTests.cpp
@@ -0,0 +1,39 @@
+#include "AppCheckTests.h"
+#include <iostream>
+#include <curl/curl.h>
+
+bool AppCheckTests::http_get_string(std::string url, std::string needle)
+{
+	bool result = false;
+	std::string readBuffer;
+	CURLcode response;
+
+	CURL* curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		//curl_easy_setopt(curl, CURLOPT_VERBOSE, true);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, AppCheckTests::write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+		response = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+	}
+
+	if (CURLE_OK == response) {
+		if (std::string::npos != readBuffer.find(needle)) {
+			result = true;
+		}
+	}
+
+	return result;
+}
+
+bool AppCheckTests::tcp_check_port(int port, std::string protocol)
+{
+	return false;
+}
+
+size_t AppCheckTests::write_data(void* contents, size_t size, size_t nmemb, void* userp)
+{
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
+}
